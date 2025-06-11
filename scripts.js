3640acc0daf20dc08d8f6acf279972901a8da7f0
Lucia <luherreros@gmail.com>
2025-06-11 16:10:10 -0300

    Página de contacto

--- scripts.js
+++ scripts.js
@@ -162,3 +162,29 @@ if (darkMode && lightMode) {
 	lightMode.addEventListener("click", changeLightMode);
 	darkMode.addEventListener("click", changeDarkMode);
 }
+
+// Validación del formulario
+
+document.addEventListener("DOMContentLoaded", function () {
+  const form = document.getElementById("formContacto");
+  const mensajeError = document.getElementById("mensajeError");
+
+  form.addEventListener("submit", function (e) {
+    e.preventDefault(); // No envía el formulario
+
+    const nombre = document.getElementById("nombre").value.trim();
+	const apellido = document.getElementById("apellido").value.trim();
+    const email = document.getElementById("email").value.trim();
+    const mensaje = document.getElementById("mensaje").value.trim();
+
+    const emailValido = /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email);
+
+    if (nombre && apellido && emailValido && mensaje) {
+      mensajeError.classList.add("hidden");
+      alert("Mensaje enviado correctamente.");
+      form.reset();
+    } else {
+      mensajeError.classList.remove("hidden");
+    }
+  });
+});
